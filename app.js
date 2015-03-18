e7cd0b3cc342ce346a3aa4f684c6b316d95040b2
Gautam Mittal <gautam@mittal.net>
2015-03-18 17:33:53 -0400

    pure recursive awesomeness

--- app.js
+++ app.js
@@ -7,16 +7,40 @@ var port = 6002;
 var connect_address = "http://www.gautam.cc:"+port; // for anyone interested in trying this out, change the address from gautam.cc to your server's IP address or domain name
 
 var wats = 0;
+var noConnect_wats = 0;
+
 
 app.get('/', function (req, res) {
+    wats++;
+
+    console.log('Wat '+wats);
+    res.send('You have imposed pure recursive awesomeness on this server.');
     
     request(connect_address); // make the GET request again
 
-    wats++;
-    console.log('Wat');
-    res.send('You have imposed pure recursive awesomeness on this server.')
+  
 })
 
+
+
+request(connect_address); // Node.js GET request recursion
+
+
+//noConnect_wat(); // engage the test to see which will complete first
+// uncomment above line to see how fast simple javascript recursion is
+
+
+function noConnect_wat() {
+    noConnect_wats++;
+    console.log('No connect Wat '+noConnect_wats);
+    
+    noConnect_wat();
+}
+
+
+setTimeout(function() {
+    throw error;
+}, 10000);
 
 
 
